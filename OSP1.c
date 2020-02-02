b51557cdc91dfaca7cdafff6d98b1b26e1879ade
Jacob Esswein <essweinjacob@gmail.com>
2020-02-02 13:03:05 -0600

    First real commit. Added fundatmental setup for project to work

---
+++ OSP1.c
@@ -0,0 +1,24 @@
+/*===============================================================================
+# Author: Jacob Esswein
+# Course: CMPSCI 4760-01 - Operating Systems
+# File Name:
+# Data Started: Febuary 2 2020
+# Date Finished: Febuary 2020
+# Purpose: The goal of this project is to become familiar with hoare
+	   enviornment while praciticing system calls such as  perror getopts.
+	
+	   Specifically, we wil traverse directories using breadth-first order
+	   as well as using command line arguments for options such as following:
+		symbolic links, printing information of the file type, print
+	   	permissions, etc.
+	 
+ 	   We will also use perror for useful error messages.
+===============================================================================*/
+#include <stdio.h>	// Standard C libary
+
+
+void main(int argc, char* argv[]){
+	// Use getopts to setup receiving command line arguments
+	// Switch statement to take case by case arguements
+	// Search directory
+}
