d938484918f8ecc0a56bf9ae6c451324f9114f41
Jacob Esswein <essweinjacob@gmail.com>
2020-02-02 13:35:51 -0600

    Added to project file. Added framework for switch statement

--- OSP1.c
+++ OSP1.c
@@ -16,9 +16,43 @@
 ===============================================================================*/
 #include <stdio.h>	// Standard C libary
 
-
 void main(int argc, char* argv[]){
 	// Use getopts to setup receiving command line arguments
-	// Switch statement to take case by case arguements
+	/* getopts prototype is (int argc, char * const argv[], const char *optstring)
+	 aka getopts(number of command line 'strings', the 'strings', the options to
+	 look for and what options to accept
+	*/
+	// Use while loop to cycle through comman line arguments/options 
+	while((int option = getopt(argc, argv, "hI:hLtpiugsdl")) != -1){
+		// Use switch case as multiple options. More efficient then if->else statements
+		switch(option){
+			// h - Print a help message and exit.
+			case 'h':
+			// In - indented by n spaces.
+			case 'I':
+			// L - Follow symbolic links, if any. Default will be to not follow any symbolic links.
+			case 'L':
+			// t - Print information on file type.
+			case 't':
+			// p - Print permission bits as rwxrwxrwx.
+			case 'p':
+			// i - Print the number of links to file in inode table.
+			case 'i':
+			// u - Print the UID associated with the file.
+			case 'u':
+			// g - Print the GID associated with the file.
+			case 'g':
+			// s - Print the size of file in bytes. If the size is larger than 1K, indicate the size in KB with a suffix K; if the size is larger than
+			// 1M, indicate the size in MB with a suffix M; if the size is larger than 1G, indicate the size in GB with a suffix G.
+			case 's':
+			// d - Show the name of last modification.
+			case 'd':
+			// l - This option will be used to print formation on the file as if the options tpiugs are all specified
+			case 'l':
+			// Default case
+			default:
+		}
+	}
+	
 	// Search directory
 }
